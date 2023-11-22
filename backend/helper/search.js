e91dc67331b7e898f1203ef54c9ab3de080f074d
teejaynam <teejaynam@gmail.com>
2023-11-21 23:21:54 -0500

    add yelp fusion search business function

--- backend/helper/search.js
+++ backend/helper/search.js
@@ -1,7 +1,5 @@
 const yelp = require('yelp-fusion');
 
-// Place holder for Yelp Fusion's API Key. Grab them
-// from https://www.yelp.com/developers/v3/manage_app
 const apiKey = 'SDzrM8FUGWFMAO8pHOpg2TWbF58Raa8e_hC7RDxjOAjue8DNYrPih-R_qsEW7hpuYGP-sda9ZPrjypl_6yHgmq_0L1QrbdfQVzhAeon_8u2gEGITOEXcuZMrXfBbZXYx';
 
 console.log("Hello, please enter a city + state/province, then enter a business")
@@ -9,8 +7,8 @@ const term = input("Enter a business");
 const location = input("Enter a location (City, State/Province")
 
 const searchRequest = {
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
+  term: term,
+  location: location
 };
 
 const client = yelp.client(apiKey);
