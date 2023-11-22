764a48c07d0337237f0f5ef60572110697b8b59b
teejaynam <teejaynam@gmail.com>
2023-11-22 17:56:09 -0500

    add input values for example and added two results

--- backend/helper/search.js
+++ backend/helper/search.js
@@ -1,10 +1,11 @@
 const yelp = require('yelp-fusion');
+const process = require('process'); 
 
 const apiKey = 'SDzrM8FUGWFMAO8pHOpg2TWbF58Raa8e_hC7RDxjOAjue8DNYrPih-R_qsEW7hpuYGP-sda9ZPrjypl_6yHgmq_0L1QrbdfQVzhAeon_8u2gEGITOEXcuZMrXfBbZXYx';
 
-console.log("Hello, please enter a city + state/province, then enter a business")
-const term = input("Enter a business");
-const location = input("Enter a location (City, State/Province")
+const term = process.argv[2]
+const location = process.argv[3]
+console.log(`You've entered : ${term} at ${location}`)
 
 const searchRequest = {
   term: term,
@@ -15,8 +16,11 @@ const client = yelp.client(apiKey);
 
 client.search(searchRequest).then(response => {
   const firstResult = response.jsonBody.businesses[0];
-  const prettyJson = JSON.stringify(firstResult, null, 4);
-  console.log(prettyJson);
+  const secondResult = response.jsonBody.businesses[1];
+  console.log(typeof secondResult)
+  console.log(firstResult);
+  console.log(secondResult);
+
 }).catch(e => {
   console.log(e);
 });
