a1c3639fa8a3f6781f75bf7f4d972dafeca55a41
Shawn He <shawnhe1991@gmail.com>
2023-11-25 15:55:28 -0500

    complete router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,46 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import LoginButton from './components/auth/login';
-import LogoutButton from './components/auth/logout';
-import Profile from './components/auth/profile';
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LoginButton from "./components/auth/login";
+import LogoutButton from "./components/auth/logout";
+import Profile from "./components/auth/profile";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   const { isAuthenticated } = useAuth0();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          Welcome to FlyBoy! 
-        </p>
-        {isAuthenticated ? (
-          <>
-            <Profile />
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {/* Edit <code>src/App.js</code> and save to reload. */}
+            Welcome to FlyBoy!
+          </p>
+          {isAuthenticated ? (
+            <>
+              <Profile />
+              <LogoutButton />
+            </>
+          ) : (
+            <LoginButton />
+          )}
+        </header>
+        <Switch>
+          <Route path="/login">
+            <LoginButton />
+          </Route>
+          <Route path="/logout">
             <LogoutButton />
-          </>
-        ) : (
-          <LoginButton />
-        )}
-
-      </header>
-    </div>
+          </Route>
+          <Route path="/flight">
+            <Profile />
+          </Route>
+          {/* Add more routes as needed */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
