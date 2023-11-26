30360e632ee891acd5ad5710f969f5f0975a2863
Charlie C <zbyabqrc@pm.me>
2023-11-26 19:01:47 +0000

    created database and seed files

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (first_name, last_name) VALUES
+  ('John', 'Doe'),
+  ('Jane', 'Smith');
---
+++ backend/db/seeds/03_areas_of_interest.sql
@@ -0,0 +1,6 @@
+-- Inserting sample data into the 'areas_of_interest' table
+INSERT INTO areas_of_interest (item_name) VALUES
+  ('Historical Site'),
+  ('Park'),
+  ('Museum'),
+  ('Shopping District');
