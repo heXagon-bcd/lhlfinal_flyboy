df60825d870ff62161b3542e1cb990d49621f047
teejaynam <teejaynam@gmail.com>
2023-11-26 13:49:05 -0500

    update search js to use require and async

--- backend/routes/helper/search.js
+++ backend/routes/helper/search.js
@@ -1,42 +1,32 @@
 const yelp = require('yelp-fusion');
-
 const apiKey = 'SDzrM8FUGWFMAO8pHOpg2TWbF58Raa8e_hC7RDxjOAjue8DNYrPih-R_qsEW7hpuYGP-sda9ZPrjypl_6yHgmq_0L1QrbdfQVzhAeon_8u2gEGITOEXcuZMrXfBbZXYx';
-
-/* const term = process.argv[2]
-const location = process.argv[3]
-console.log(`You've entered : ${term} at ${location}`)
-
-const searchRequest = {
-  term: term,
-  location: location
-}; */
 
 const client = yelp.client(apiKey);
 
+async function search(term, location) {
+  try {
+    const searchRequest = {
+      term: term,
+      location: location
+    };
 
-export function Search(term, location) { 
-  const searchRequest = {
-    term: term,
-    location: location
-  };
-
-  client.search(searchRequest).then(response => {
-    const firstResult = response.jsonBody.businesses[0];
+    const response = await client.search(searchRequest);
     const secondResult = response.jsonBody.businesses[1];
-  
+
     const resultObj = {
-      id : secondResult.id,
-      name : secondResult.name,
-      image : secondResult.image_url,
-      categories : secondResult.categories,
-      rating : secondResult.rating,
-      location : secondResult.location.address1
-    }
-  
-    //console.log(response.jsonBody.businesses);
-    return (resultObj);
-  }).catch(e => {
-    //console.log(e);
-    return(e);
-  });
+      id: secondResult.id,
+      name: secondResult.name,
+      image: secondResult.image_url,
+      categories: secondResult.categories,
+      rating: secondResult.rating,
+      location: secondResult.location.address1
+    };
+
+    return resultObj;
+  } catch (error) {
+    console.error('Error in Yelp API request:', error);
+    throw error; // Rethrow the error so the caller can handle it
+  }
 }
+
+module.exports = { search };
