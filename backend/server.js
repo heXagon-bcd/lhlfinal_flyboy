59163a1b78ee9acf3314cbe4ba5800e500f2ac44
teejaynam <teejaynam@gmail.com>
2023-11-26 13:49:27 -0500

    add post handler route to yelp fusion api

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,18 @@ require("dotenv").config();
 // Web server config
 const express = require("express");
 const morgan = require("morgan");
-
+const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 const app = express();
+const {search} = require("./routes/helper/search")
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -34,6 +37,21 @@ app.use("/users", usersRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.post("/api/flights", async(req, res) => {
+  try {
+    const terms = req.body.interest1;
+    const location = req.body.destination;
+
+    const result = await search(terms, location);
+
+    console.log(result);
+    res.json({ result });
+  } catch (error) {
+    console.error("Error in /api/flights route:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(PORT, () => {
