523a2a7660cb2810053a3056afb4c5493d2dc416
teejaynam <teejaynam@gmail.com>
2023-12-04 21:13:17 -0500

    add date tag

--- frontend/src/components/form/TravelFormModal.jsx
+++ frontend/src/components/form/TravelFormModal.jsx
@@ -1,11 +1,13 @@
 export const TravelFormModal = ({ data }) => {
     return (
-        <div>
+        <div style={{display:"flex", flexWrap: "wrap"}}>
             {data.map((item, index) => (
                 <div key={index}>
-                    <h1>{item.name}</h1>
+                    <h1>{item.date}</h1>
+                    <h2>{item.name}</h2>
                     <img src={item.image} alt={item.name} />
                     <p>Rating: {item.rating}</p>
+                    <p>Pricing: {item.price}</p>
                     <p>Location: {item.location}</p>
                 </div>
             ))}
