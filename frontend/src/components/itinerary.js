3c9b82644d7ed92cd3c4a8f75d8a8806550260f8
teejaynam <teejaynam@gmail.com>
2023-12-05 20:12:19 -0500

    first commit

--- frontend/src/components/itinerary.js
+++ frontend/src/components/itinerary.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from "react";
 import airplane from "../../src/flight-loader.svg";
 
 export default function Itinerary() {
-<<<<<<< HEAD
-  return <h1>"hello"</h1>;
-=======
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState([]);
 
@@ -29,5 +26,4 @@ export default function Itinerary() {
       })}
     </div>
   );
->>>>>>> 07bd56543dc67646d58bf6781b5a407acbb1297d
 }
