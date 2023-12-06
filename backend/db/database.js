3e89434e03de5c6dbb02c7a399c2ec58fa888e91
Shawn He <shawnhe1991@gmail.com>
2023-12-06 17:00:49 -0500

    build frame work to add to your itinerary

--- backend/db/database.js
+++ backend/db/database.js
@@ -19,7 +19,8 @@ const findItinerary = (userid) => {
 };
 
 const saveItinerary = (json) => {
-  const queryString = ''
+  const queryString =
+    "INSERT INTO itinerary (users_id, trip_name, start_location, end_location, start_date, end_date, flight_price, hotel_cost, hotel_description, interest_name, interest_location, interest_image, interest_rating) VALUES (1, )";
 };
 
 getUser();
