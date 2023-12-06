553385031a0fbbadcb97c3805e09066c4dbb071d
teejaynam <teejaynam@gmail.com>
2023-12-05 20:11:34 -0500

    add function to group interests by date

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -2,56 +2,62 @@ import { useEffect, useState } from 'react';
 import { TravelForm } from './TravelForm';
 import { TravelFormModal } from './TravelFormModal';
 import { FlightHotelModal } from './FlightHotelModal';
-<<<<<<< HEAD
-=======
 import { Spinner } from '../spinner/spinner';
->>>>>>> 07bd56543dc67646d58bf6781b5a407acbb1297d
-import '../../style/Form.css'
+import '../../style/Form.css';
 
 export const Form = () => {
-    const [submittedData, setSubmittedData] = useState([]);
+  const [submittedData, setSubmittedData] = useState([]);
 
-    const handleFormSubmit = (data) => {
-        setSubmittedData((prevData) => [...prevData, data]);
-        console.log("handleFormSubmit", data[0].yelpApi)
-    }
+  const handleFormSubmit = (data) => {
+    setSubmittedData((prevData) => [...prevData, data]);
+    console.log("handleFormSubmit", data[0].yelpApi);
+  };
 
-    console.log("response data", JSON.stringify(submittedData));//return
-    // console.log("response data non json", submittedData[0][0].yelpApi);//return
-    const EMPTY = "EMPTY";
-    const SHOW = "SHOW";
+  console.log("response data", JSON.stringify(submittedData));//return
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
 
-<<<<<<< HEAD
+  const api = submittedData?.[0]?.[0]?.yelpApi;
 
-    return (
+  let groupedByDate;
+
+  if (api) {
+    groupedByDate = api.reduce((acc, item) => {
+      const date = item.date;
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push(item);
+      return acc;
+    }, {});
+  }
+
+  return (
+    <div>
+      {submittedData.length === 0 && (
         <div>
-          { submittedData.length === 0 && (<div>
-            <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>Travel App</h1>
-            <TravelForm onSubmit={handleFormSubmit} />
-          </div>)}
-
-            {submittedData.length > 0 && (<div>
-            <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>Travel App</h1>
-            <TravelForm onSubmit={handleFormSubmit} />
-=======
-    return (
-        <div>
-          { submittedData.length === 0 && (<div>
-            <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>Travel App</h1>
-            <TravelForm onSubmit={handleFormSubmit} />
-          </div>)}
-
-            {submittedData.length > 0 && (<div>
-            <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>Travel App</h1>
->>>>>>> 07bd56543dc67646d58bf6781b5a407acbb1297d
-            <h1> Flight & Hotel Prices</h1>
-            {<FlightHotelModal data = {submittedData[0]}/>}
-            <h1>Interests</h1>
-            {submittedData[0][0].yelpApi.map((data) => (
-                <TravelFormModal data={data} />
-            ))}
-
-          </div>)}
+          <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>Travel App</h1>
+          <TravelForm onSubmit={handleFormSubmit} />
         </div>
-    );
+      )}
+
+      {submittedData.length > 0 && (
+        <div>
+          <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>Travel App</h1>
+          <h1> Flight & Hotel Prices</h1>
+          {<FlightHotelModal data={submittedData[0]} />}
+          <h1>Interests</h1>
+          {groupedByDate &&
+            Object.entries(groupedByDate).map(([date, dataGroup]) => (
+              <div key={date}>
+                <h2>{date}</h2>
+                {dataGroup.map((data) => (
+                  <TravelFormModal key={data.id} data={data} />
+                ))}
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
+  );
 };
