50f2cfd8a6fbd0dcb6f5d14a4bec0c7d0b03cd68
Shawn He <shawnhe1991@gmail.com>
2023-12-06 13:04:12 -0500

    complete select user query

---
+++ backend/db/database.js
@@ -0,0 +1,22 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "shawnhe",
+  password: "shawnhe",
+  host: "localhost",
+  database: "flyboy",
+});
+
+const getUser = () => {
+  const queryString = " SElECT * FROM USERS;";
+  return pool.query(queryString).then((result) => {
+    console.log("getUser Query", result.rows);
+  });
+};
+
+const findItinerary = (userid) => {
+  const queryString = ''
+}
+
+getUser();
+module.exports = { getUser };
