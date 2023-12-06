c0c250c23f467b9e3d43cdbb4aa7fd9df9fc857d
teejaynam <teejaynam@gmail.com>
2023-12-05 20:12:00 -0500

    update component to show relevant data

--- frontend/src/components/form/TravelForm.jsx
+++ frontend/src/components/form/TravelForm.jsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import axios from "axios";
-import "../../style/TravelForm.css";
-=======
 import { useFormik } from 'formik';
 import React, {useEffect, useState} from 'react';
 import * as Yup from 'yup';
 import axios from 'axios';
 import '../../style/TravelForm.css'
 import airplane from "../../flight-loader.svg";
->>>>>>> 07bd56543dc67646d58bf6781b5a407acbb1297d
 
 export const TravelForm = ({ onSubmit }) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -55,16 +48,6 @@ export const TravelForm = ({ onSubmit }) => {
   });
 
   return (
-<<<<<<< HEAD
-    <form className="search-form" onSubmit={formik.handleSubmit}>
-      <div className="title">
-        <h4>Enter Your Trip Details</h4>
-      </div>
-      <div className="upper-form">
-        <div className="left-column">
-          <div className="input-container">
-            {/* <label htmlFor="destination">Destination:</label> */}
-=======
 <div>
       {isLoading ? (
         <img src={airplane} alt="Loading" />
@@ -76,15 +59,10 @@ export const TravelForm = ({ onSubmit }) => {
 
           <div>
             <label htmlFor="destination">Destination:</label>
->>>>>>> 07bd56543dc67646d58bf6781b5a407acbb1297d
             <input
               type="text"
               id="destination"
               name="destination"
-<<<<<<< HEAD
-              placeholder="Enter a Destination"
-=======
->>>>>>> 07bd56543dc67646d58bf6781b5a407acbb1297d
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               value={formik.values.destination}
@@ -94,123 +72,6 @@ export const TravelForm = ({ onSubmit }) => {
             )}
           </div>
 
-<<<<<<< HEAD
-          <div className="input-container">
-            <div className="departure-heading">
-              <label htmlFor="departureDate">Departure date:</label>
-            </div>
-            <input
-              className="date"
-              type="date"
-              id="departureDate"
-              name="departureDate"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.departureDate}
-            />
-            {formik.touched.departureDate && formik.errors.departureDate && (
-              <div>{formik.errors.departureDate}</div>
-            )}
-          </div>
-        </div>
-
-        <div className="right-column">
-          <div className="input-container">
-            {/* <label htmlFor="locationFromDeparture">
-            Location from Departure:
-          </label> */}
-            <input
-              type="text"
-              id="locationFromDeparture"
-              name="locationFromDeparture"
-              placeholder="Enter Departure Location"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.locationFromDeparture}
-            />
-            {formik.touched.locationFromDeparture &&
-              formik.errors.locationFromDeparture && (
-                <div>{formik.errors.locationFromDeparture}</div>
-              )}
-          </div>
-
-          <div className="input-container">
-            <div className="return-heading">
-              <label htmlFor="returnDate">Return Date:</label>
-            </div>
-            <input
-              className="date"
-              type="date"
-              id="returnDate"
-              name="returnDate"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.returnDate}
-            />
-            {formik.touched.returnDate && formik.errors.returnDate && (
-              <div>{formik.errors.returnDate}</div>
-            )}
-          </div>
-        </div>
-      </div>
-      <div className="lower-form">
-        <div className="heading">
-          <label>Interests: Italian Food, Golf, Hiking, Museums, etc.</label>
-        </div>
-        <div className="input-container">
-          {/* <label htmlFor="interest1">Interest 1:</label> */}
-          <input
-            type="text"
-            id="interest1"
-            name="interest1"
-            placeholder="Interest 1"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.interest1}
-          />
-          {formik.touched.interest1 && formik.errors.interest1 && (
-            <div>{formik.errors.interest1}</div>
-          )}
-        </div>
-
-        <div className="input-container">
-          {/* <label htmlFor="interest2">Interest 2:</label> */}
-          <input
-            type="text"
-            id="interest2"
-            name="interest2"
-            placeholder="Interest 2"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.interest2}
-          />
-          {formik.touched.interest2 && formik.errors.interest2 && (
-            <div>{formik.errors.interest2}</div>
-          )}
-        </div>
-
-        <div className="input-container">
-          {/* <label htmlFor="interest3">Interest 3:</label> */}
-          <input
-            type="text"
-            id="interest3"
-            name="interest3"
-            placeholder="Interest 3"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.interest3}
-          />
-          {formik.touched.interest3 && formik.errors.interest3 && (
-            <div>{formik.errors.interest3}</div>
-          )}
-        </div>
-
-        <button className="search-button" type="submit">
-          SUBMIT
-        </button>
-      </div>
-    </form>
-=======
           <div className="upper-form">
             <div className="left-column">
               <div className="input-container">
@@ -313,6 +174,5 @@ export const TravelForm = ({ onSubmit }) => {
         </form>
       )}
     </div>
->>>>>>> 07bd56543dc67646d58bf6781b5a407acbb1297d
   );
 };
