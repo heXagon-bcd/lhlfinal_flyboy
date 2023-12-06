0b524049d120d1a0cdf97d4b8b7a235a6f26a48a
teejaynam <teejaynam@gmail.com>
2023-12-05 20:12:11 -0500

    update component to show relevant data

--- frontend/src/components/form/TravelFormModal.jsx
+++ frontend/src/components/form/TravelFormModal.jsx
@@ -1,31 +1,12 @@
-<<<<<<< HEAD
 export const TravelFormModal = ({ data }) => {
-    return (
-        <div style={{display:"flex", flexWrap: "wrap"}}>
-            {data.map((item, index) => (
-                <div key={index}>
-                    <h1>{item.date}</h1>
-                    <h2>{item.name}</h2>
-                    <img src={item.image} alt={item.name} />
-                    <p>Rating: {item.rating}</p>
-                    <p>Pricing: {item.price}</p>
-                    <p>Location: {item.location}</p>
-                </div>
-            ))}
-        </div>
-    );
+  console.log(data)
+  return (
+    <div style={{display:"flex"}}>
+      <h2>{data.name}</h2>
+      <img src={data.image} alt={data.name} />
+      <p>Rating: {data.rating}</p>
+      <p>Pricing: {data.price}</p>
+      <p>Location: {data.location}</p>
+    </div>
+  );
 };
-=======
-export const TravelFormModal = ({ data }) => {
-
-  console.log("Travel form data", data)
-    return (
-        <div>
-              <h2>{data.name}</h2>
-              <img src={data.image} alt={data.name} />
-              <p>Rating: {data.rating}</p>
-              <p>Location: {data.location}</p>
-        </div>
-    );
-};
->>>>>>> 07bd56543dc67646d58bf6781b5a407acbb1297d
