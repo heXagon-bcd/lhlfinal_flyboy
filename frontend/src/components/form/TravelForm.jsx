8d9788a8216c6daa7e2bcd0ef266bbed4c3d77a5
Charlie C <zbyabqrc@pm.me>
2023-12-07 04:46:59 +0000

    updated travel form UI

--- frontend/src/components/form/TravelForm.jsx
+++ frontend/src/components/form/TravelForm.jsx
@@ -1,12 +1,11 @@
+import React from "react";
 import { useFormik } from "formik";
-import React, { useEffect, useState } from "react";
 import * as Yup from "yup";
 import axios from "axios";
 import "../../style/TravelForm.css";
 import airplane from "../../flight-loader.svg";
 
 export const TravelForm = ({ onSubmit }) => {
-  const [isLoading, setIsLoading] = useState(false);
   const formik = useFormik({
     initialValues: {
       tripName: "",
@@ -20,10 +19,8 @@ export const TravelForm = ({ onSubmit }) => {
     },
     validationSchema: Yup.object({
       tripName: Yup.string().required("Trip name is required"),
-      destination: Yup.string().required("Destination is required"),
-      locationFromDeparture: Yup.string().required(
-        "Departure location is required"
-      ),
+      destination: Yup.string().required("Destination city is required"),
+      locationFromDeparture: Yup.string().required("Departure city is required"),
       departureDate: Yup.date().required("Departure date required"),
       returnDate: Yup.date().required("Return date required"),
       interest1: Yup.string().required("Interest 1 is required"),
@@ -31,174 +28,161 @@ export const TravelForm = ({ onSubmit }) => {
       interest3: Yup.string().required("Interest 3 is required"),
     }),
     onSubmit: async (values) => {
-      setIsLoading(true);
       try {
-        const res = await axios.post(
-          "http://localhost:8080/api/flight",
-          values
-        );
+        const res = await axios.post("http://localhost:8080/api/flight", values);
         onSubmit(res.data);
-        // console.log("form submission..", res.data[0].yelpApi);
-        console.log("res.data", res.data);
-        // console.log("form submission values", values)
       } catch (error) {
         console.error("Error:", error);
-      } finally {
-        setIsLoading(false);
       }
-    }, //use promise instead
+    },
   });
 
   return (
     <div className="outer-form-container">
-      {isLoading ? (
-        <div className="loading-screen-container">
-          <img src={airplane} alt="Loading" />
-          <p>Loading...</p>
-        </div>
-      ) : (
+      {!formik.isSubmitting ? (
         <form className="search-form" onSubmit={formik.handleSubmit}>
           <div className="upper-form">
             <div className="title">
               <h4>Enter Your Trip Details</h4>
             </div>
-              <div className="input-container">
-                <input
-                  type="text"
-                  id="tripName"
-                  name="tripName"
-                  placeholder="Enter Trip Name"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.tripName}
-                  className={`formik ${formik.touched.tripName && formik.errors.tripName ? 'formik-error' : ''}`}
-                />
-                {formik.touched.tripName && formik.errors.tripName && (
-                  <div className="formik-error">{formik.errors.tripName}</div>
-                )}
-              </div>
+            <InputField
+              id="tripName"
+              name="tripName"
+              placeholder="Enter Trip Name"
+              type="text"
+              value={formik.values.tripName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              touched={formik.touched.tripName}
+              error={formik.errors.tripName}
+            />
             <div className="columns">
               <div className="left-column">
-                <div className="input-container">
-                  <input
-                    type="text"
-                    id="destination"
-                    name="destination"
-                    placeholder="Destination City"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.destination}
-                    className={`formik ${formik.touched.destination && formik.errors.destination ? 'formik-error' : ''}`}
-                    />
-                    {formik.touched.destination && formik.errors.destination && (
-                      <div className="formik-error">{formik.errors.destination}</div>
-                  )}
-                </div>
-
-                <div className="input-container">
-                  <input
-                    type="date"
-                    id="departureDate"
-                    name="departureDate"
-                    className="date"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.departureDate}
-                  />
-                  {formik.touched.departureDate &&
-                    formik.errors.departureDate && (
-                      <div>{formik.errors.departureDate}</div>
-                    )}
-                </div>
+                <InputField
+                  id="destination"
+                  name="destination"
+                  placeholder="Destination City"
+                  type="text"
+                  value={formik.values.destination}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  touched={formik.touched.destination}
+                  error={formik.errors.destination}
+                />
+                <InputField
+                  id="departureDate"
+                  name="departureDate"
+                  placeholder="Departure Date"
+                  type="date"
+                  value={formik.values.departureDate}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  touched={formik.touched.departureDate}
+                  error={formik.errors.departureDate}
+                />
               </div>
 
               <div className="right-column">
-                <div className="input-container">
-                  <input
-                    type="text"
-                    id="locationFromDeparture"
-                    name="locationFromDeparture"
-                    placeholder="City of Departure"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.locationFromDeparture}
-                  />
-                  {formik.touched.locationFromDeparture &&
-                    formik.errors.locationFromDeparture && (
-                      <div>{formik.errors.locationFromDeparture}</div>
-                    )}
-                </div>
-
-                <div className="input-container">
-                  <input
-                    type="date"
-                    id="returnDate"
-                    name="returnDate"
-                    className="date"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.returnDate}
-                  />
-                  {formik.touched.returnDate && formik.errors.returnDate && (
-                    <div>{formik.errors.returnDate}</div>
-                  )}
-                </div>
+                <InputField
+                  id="locationFromDeparture"
+                  name="locationFromDeparture"
+                  placeholder="City of Departure"
+                  type="text"
+                  value={formik.values.locationFromDeparture}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  touched={formik.touched.locationFromDeparture}
+                  error={formik.errors.locationFromDeparture}
+                />
+                <InputField
+                  id="returnDate"
+                  name="returnDate"
+                  placeholder="Return Date"
+                  type="date"
+                  value={formik.values.returnDate}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  touched={formik.touched.returnDate}
+                  error={formik.errors.returnDate}
+                />
               </div>
             </div>
           </div>
 
           <div className="lower-form">
-            <div className="input-container">
-              <input
-                type="text"
-                id="interest1"
-                name="interest1"
-                placeholder="Interest 1"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.interest1}
-              />
-              {formik.touched.interest1 && formik.errors.interest1 && (
-                <div>{formik.errors.interest1}</div>
-              )}
-            </div>
+            <InputField
+              id="interest1"
+              name="interest1"
+              placeholder="Interest 1"
+              type="text"
+              value={formik.values.interest1}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              touched={formik.touched.interest1}
+              error={formik.errors.interest1}
+            />
+            <InputField
+              id="interest2"
+              name="interest2"
+              placeholder="Interest 2"
+              type="text"
+              value={formik.values.interest2}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              touched={formik.touched.interest2}
+              error={formik.errors.interest2}
+            />
+            <InputField
+              id="interest3"
+              name="interest3"
+              placeholder="Interest 3"
+              type="text"
+              value={formik.values.interest3}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              touched={formik.touched.interest3}
+              error={formik.errors.interest3}
+            />
 
-            <div className="input-container">
-              <input
-                type="text"
-                id="interest2"
-                name="interest2"
-                placeholder="Interest 2"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.interest2}
-              />
-              {formik.touched.interest2 && formik.errors.interest2 && (
-                <div>{formik.errors.interest2}</div>
-              )}
-            </div>
-
-            <div className="input-container">
-              <input
-                type="text"
-                id="interest3"
-                name="interest3"
-                placeholder="Interest 3"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.interest3}
-              />
-              {formik.touched.interest3 && formik.errors.interest3 && (
-                <div>{formik.errors.interest3}</div>
-              )}
-            </div>
-
-            <button className="search-button" type="submit">
+            <button className="form-submit-button" type="submit">
               SUBMIT
             </button>
           </div>
         </form>
+      ) : (
+        <div className="loading-screen-container">
+          <img src={airplane} alt="Loading" />
+          <p>Loading...</p>
+        </div>
       )}
     </div>
   );
 };
+
+const InputField = ({
+  id,
+  name,
+  placeholder,
+  type,
+  value,
+  onChange,
+  onBlur,
+  touched,
+  error,
+}) => (
+  <div className="input-container">
+    <input
+      type={type}
+      id={id}
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+      onBlur={onBlur}
+      value={value}
+      className={`formik ${touched && error ? "formik-error" : ""}`}
+    />
+    {touched && error && <div className="formik-error">{error}</div>}
+  </div>
+);
+
+export default TravelForm;
